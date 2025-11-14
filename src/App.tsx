0c5799cee033accff57733a52265a03d234ba86b
Thalitalima23 <thalitalima23@gmail.com>
2025-11-14 10:11:42 -0300

    :hibiscus:Estilização do Cadastro e Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./components/footer/Footer"
 import Navbar from "./components/navbar/Navbar"
-import Home from "./pages/home/Home"
 import Cadastro from "./pages/cadastro/Cadastro"
+import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
 
 
@@ -17,9 +17,8 @@ function App() {
         <Navbar />
         <div className="min-h-[80vh]">
         <Routes>
-           <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
            <Route path="/home" element={<Home />} />
-           <Route path="/login" element={<Login />} />
            <Route path="/cadastro" element={<Cadastro />} />
         </Routes>
         </div>
