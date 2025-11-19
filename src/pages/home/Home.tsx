91bd44beece7179714ad681bd127ac559d9b3cad
Thalitalima23 <thalitalima23@gmail.com>
2025-11-19 15:18:38 -0300

    :sparkles: Consumo do recurso Postagem

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,56 +1,33 @@
+import ListaPostagens from "../../components/postagem/listapostagens/ListaPostagens";
+import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem";
 
 function Home() {
-
-
-
     return (
-        /*Essa é o container*/
-        <div
+        <>
+            <div className="bg-teal-800 flex justify-center">
+                <div className="container grid grid-cols-2 text-white">
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className="text-5xl font-bold">Seja Bem Vinde!</h2>
+                        <p className="text-xl">Expresse aqui seus pensamentos e opiniões :):</p>
 
-            className="bg-teal-800 flex justify-center"
+                        <div className="flex justify-around gap-4">
+                             <ModalPostagem />
+                        </div>
+                    </div>
 
-
-        >
-            {/*Grid que divide a tela em 2 colunas*/}
-
-            <div
-                className="container grid grid-cols-2 text-white"
-            >
-                {/*Essa é minha coluna da esquerda*/}
-                <div
-                    className="flex flex-col gap-4 items-center justify-center py-4"
-
-                >
-                    <h2
-                        className="text-5xl font-bold"
-                    >Seja Bem Vinde!</h2>
-                    <p
-                        className="text-xl"
-                    > Expresse aqui seus pensamentos e opinões :): </p>
-
-                    {/*Essa é um link/botao*/}
-                    <div
-
-                        className="flex justify-around gap-4"
-                    >
-                        <div
-                            className="rounded text-white border-white border-solid border-2 py-2 px-4"
-                        >Nova Postagem</div>
+                    <div className="flex justify-center">
+                        <img
+                            src="https://ik.imagekit.io/Thalima23/Copilot_20251113_114541.png?updatedAt=1763045157600"
+                            alt="Imagem da Página home"
+                            className="w-2/3"
+                        />
                     </div>
                 </div>
-                {/*Essa é minha coluna da direita*/}
-                <div
-                    className="flex justify-center"
-                >
-                    <img
-                        src="https://ik.imagekit.io/Thalima23/Copilot_20251113_114541.png?updatedAt=1763045157600"
-                        alt="Imagem da Página home"
-                        className="w-2/3"
-                    />
-                </div>
             </div>
-        </div>
-    )
+
+            <ListaPostagens />
+        </>
+    );
 }
 
-export default Home
+export default Home;
